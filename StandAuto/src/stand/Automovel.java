01ddcb64ecee8b4f277e2168b486e80db42d5d12
ATeixeira22 <alexandrefmt@gmail.com>
2018-11-14 20:51:02 +0000

    La La

---
+++ StandAuto/src/stand/Automovel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stand;
+
+/**
+ *
+ * @author Xano
+ */
+public class Automovel {
+    
+    String marca;
+    int ano;
+    int peso;
+    double consumo;
+    double preco;
+
+    public Automovel(String marca, int ano, int peso, double consumo, double preco) {
+        this.marca = marca;
+        this.ano = ano;
+        this.peso = peso;
+        this.consumo = consumo;
+        this.preco = preco;
+    }
+    
+    
+}
+
+
